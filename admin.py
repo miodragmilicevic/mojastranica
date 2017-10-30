26715c6c7b2d6f0bb796a8981d1d729af45828fa
miodragmilicevic <33198672+miodragmilicevic@users.noreply.github.com>
2017-10-30 18:48:24 +0100

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','created_date')
+admin.site.register(Post,PostAdmin)
+
+# Register your models here.
